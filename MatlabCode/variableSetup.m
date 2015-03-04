1105b3c5a816be68cb2176186359fea8eec600c2
Brisa Davis <brisadavis@Brisas-MacBook-Pro.local>
2015-03-03 22:30:20 -0800

    Adding Matlab code and generated images for AW model

---
+++ MatlabCode/variableSetup.m
@@ -0,0 +1,12 @@
+function y = variableSetup
+
+global v_star rho_star p pprime rho
+
+% Declaring various starting values
+v_star = [0.2 .5 .8];
+rho_star = [0.2 .06 .1];
+gamma = 0.2;
+
+p = @(x) x^gamma;
+pprime = @(x) gamma*x^(gamma-1);
+rho = 0:0.001:5;
